7fac5cbf3bd3d0e2e58351e9c9c05281e48901aa
Luckas Cândido <luckas.candido@gmail.com>
2025-01-02 11:47:23 -0300

    creating the database connections and ports

--- src/api/service.go
+++ src/api/service.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Erro ao carregar o arquivo .env:", err)
+		return
+	}
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	port := os.Getenv("APP_PORT")
+	appAddress := fmt.Sprintf("localhost:%s", port)
+	e.Logger.Fatal(e.Start(appAddress))
 }
